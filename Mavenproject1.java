543fcdb4f8fffa74d182eadb5aa4b799f235cd37
Raque <rchguemez@gmail.com>
2026-06-03 20:46:07 -0600

    Add files via upload

---
+++ Mavenproject1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Cliente Cliente1 = new Cliente();
+       
+        
+        Cliente1.cedula =JOptionPane.showInputDialog("Ingrese su cedula");
+        Cliente1.nombre=JOptionPane.showInputDialog("Ingrese su cedula");
+        
+       
+
+    }
+    
+     public static void CrearVehiculo(){
+     Vehiculo num1 = new Vehiculo();
+     num1.marca="hyundai";
+     
+     
+     CrearVehiculo();
+     
+     
+    }
+     
+    
+}
+
+   
+  
---
+++ Vehiculo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Vehiculo {
+    public String marca;
+    public int anio;
+    public String placa;
+    
+    public void entrenar(){};
+}
